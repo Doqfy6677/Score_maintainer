123790da180779020cc371905501b719702a3cd8
Doqfy6677 <Doqfypvt@gmail.com>
2025-07-02 12:58:49 +0530

    Delete CricketScoreTracker directory

--- CricketScoreTracker/index.py
+++
@@ -1,7 +0,0 @@
-from app import app
-
-# This file is needed for Vercel deployment
-# It imports the Flask app from app.py and makes it available to Vercel
-
-if __name__ == '__main__':
-    app.run()
--- CricketScoreTracker/vercel_app.py
+++
@@ -1,5 +0,0 @@
-from app import app
-
-# This is the handler that Vercel calls
-def handler(request, context):
-    return app(request, context)
